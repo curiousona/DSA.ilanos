9100e525236e4a30e9afa6d0517a1554e32c145b
Sonali singh <101523856+curiousona@users.noreply.github.com>
2024-02-13 20:45:24 +0000

    Computing power

---
+++ .devcontainer/Mathematics/computing_power.cpp
@@ -0,0 +1,17 @@
+int power(int x, int n)
+{
+    if (n == 0)
+    {
+        return 1;
+    }
+    int temp = power(x, n / 2);
+    temp = temp * temp;
+    if (n % 2 == 0)
+    {
+        return temp;
+    }
+    else
+    {
+        return temp * x;
+    }
+}
